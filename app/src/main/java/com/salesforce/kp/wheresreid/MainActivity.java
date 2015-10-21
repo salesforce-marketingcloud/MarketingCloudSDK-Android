5524a9da575dddc5a8af74cb7882841324429465
Gerson Da Cunha <gerson@oktana.io>
2015-10-21 15:35:09 -0200

    Ui tweaks

--- app/src/main/java/com/salesforce/kp/wheresreid/MainActivity.java
+++ app/src/main/java/com/salesforce/kp/wheresreid/MainActivity.java
@@ -2,11 +2,7 @@ package com.salesforce.kp.wheresreid;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -26,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_main);
     }
 
     @Override
